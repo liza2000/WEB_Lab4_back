b60ae7ca309eb5e20412339a827115d5eba4a936
Елизавета <medvedeva_elizaveta2000@mail.ru>
2020-05-25 15:31:36 +0300

    MBeans

--- src/main/java/main/java/model/Calculator.java
+++ src/main/java/main/java/model/Calculator.java
@@ -4,9 +4,9 @@ package main.java.model;
 public class Calculator {
 
     private boolean isInArea(double x, double y, double r) {
-        if (x>=0) return y>=0&&Math.hypot(x,y)<=r||(x==0&&Math.abs(y)<=r);
-        if (y>0) return Math.abs(2*y)<=r&&Math.abs(x)<=r;
-        return Math.abs(x)+Math.abs(y)<=r;
+        if (x >= 0) return y >= 0 && Math.hypot(x, y) <= r || (x == 0 && Math.abs(y) <= r);
+        if (y > 0) return Math.abs(2 * y) <= r && Math.abs(x) <= r;
+        return Math.abs(x) + Math.abs(y) <= r;
     }
 
     public boolean isInArea(Point point) {
