598c6919f741d8528401070f7181590d2f2422b8
Елизавета <medvedeva_elizaveta2000@mail.ru>
2020-05-27 21:49:29 +0300

    MBeans

--- src/main/java/main/java/mbeans/SquareCalculator.java
+++ src/main/java/main/java/mbeans/SquareCalculator.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class SquareCalculator implements SquareCalculatorMBean {
     private double square;
+    @Override
     public void setSquare(double r){
         square = r*r*(Math.PI/4 + 1);
     }
--- src/main/java/main/java/mbeans/SquareCalculatorMBean.java
+++ src/main/java/main/java/mbeans/SquareCalculatorMBean.java
@@ -2,4 +2,5 @@ package main.java.mbeans;
 
 public interface SquareCalculatorMBean {
     double getSquare();
+    void setSquare(double r);
 }
